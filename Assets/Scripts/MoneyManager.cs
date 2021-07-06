88462b55b191f4c8e9a1bdc972ac2618de29c8d6
Ivan Siluianov <ivan.siluianov@gmail.com>
2021-07-06 17:37:09 +0200

    alpha

--- Assets/Scripts/MoneyManager.cs
+++ Assets/Scripts/MoneyManager.cs
@@ -5,32 +5,74 @@ using UnityEngine.UI;
 
 public class MoneyManager : MonoBehaviour
 {
-    private Player player;
+    private Player _player;
     public GameObject otherGameObject;
 
     public Text moneyText;
     public int money;
     public int moneyStorage;
+    public Text moneyStorageText;
 
     public GameObject secondOtherGameObject;
-    private GameOver gameOver;
+    private GameOver _gameOver;
+
+    public int speedUpQuantity;
+    public int shootSpeedUpQuantity;
+    public int shieldQunatity;
+
+    private float _timeLeft = 0.5f;
+
+    private bool _isCounted;
     private void Update()
     {
-        gameOver = secondOtherGameObject.GetComponent<GameOver>();
+        _gameOver = secondOtherGameObject.GetComponent<GameOver>();
 
         if (otherGameObject != null)
         {
-            player = otherGameObject.GetComponent<Player>();
+            _player = otherGameObject.GetComponent<Player>();
 
-            money = player.coinCount;
+            money = _player.coinCount;
 
             moneyText.text = $"{money}";
         }
 
-        if (gameOver.isRestarted)
+        if (_gameOver.isRestarted && _isCounted == false)
         {
             moneyStorage += money;
+            moneyStorageText.text = $"{moneyStorage}";
             money = 0;
+            _isCounted = true;
+
+            _timeLeft -= Time.deltaTime;
+            if (_timeLeft < 0)
+            {
+                _isCounted = false;
+            }
+        }
+    }
+
+    public void BuySomething(string productName)
+    {
+        if (moneyStorage >= 50)
+        {
+            moneyStorage -= 50;
+            if (productName == "SpeedUp")
+            {
+                speedUpQuantity++;
+            }
+            else if (productName == "ShootSpeedUp")
+            {
+                shootSpeedUpQuantity++;
+            }
+            else if (productName == "Shield")
+            {
+                shieldQunatity++;
+            }
+
+        }
+        else
+        {
+            Debug.Log("Где деньги, Зин?");
         }
     }
 }
